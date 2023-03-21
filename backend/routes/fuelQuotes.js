7711788677ded6f183f3afbdc8bbdd07dc885542
Rodolfo Chavez <Chavezrodolfo522@gmail.com>
2023-03-21 11:18:30 -0500

    got query to work

--- backend/routes/fuelQuotes.js
+++ backend/routes/fuelQuotes.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/fuelQuote', (req, res) => {
+// POST request handler for fuel quotes
+router.post('/', (req, res) => {
   console.log('Inside fuelQuote post request handler');
   const { gallons, address, deliveryDate, pricePerGallon } = req.body;
 
-  console.log('Request body:', req.body);
+  console.log('User inputs:', req.body);
   
   // Do something with the data, e.g. calculate the fuel quote
   
   // Send the response back to the frontend
-  res.json({ /* your response data */ });
+  res.json({ message: 'got it' });
 });
 
+// New route to display user inputs as JSON
+router.get('/userInputs', (req, res) => {
+  // Return the user inputs in the request body as JSON
+  res.json(req.body);
+});
 
 module.exports = router;
