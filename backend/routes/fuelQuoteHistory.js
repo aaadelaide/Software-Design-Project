c92139110abccb4da5b4db364b4b59904656ebc1
Larroyo <95649169+DeLuci@users.noreply.github.com>
2023-04-02 19:44:20 -0500

    Added coverage and testing for fuelQuoteHistory

--- backend/routes/fuelQuoteHistory.js
+++ backend/routes/fuelQuoteHistory.js
@@ -5,6 +5,11 @@ const router = express.Router();
 function getFuelQuoteHistory(req, res) {
     const email = req.query.email;
     connection.query(`SELECT gallons, address, deliveryDate, pricePerGallon, gallons * pricePerGallon AS totalAmount FROM fuelQuotes WHERE email = '${email}'`, function(error, historyRecord, fields) {
+        
+        if (error) {
+            // console.error(error)
+            res.status(500).json({error: 'Failed to retrieve fuel quote history'})
+        } 
         res.setHeader('Content-Type', 'application/json'); // set the Content-Type header explicitly
         res.status(200).json(historyRecord);
     });
